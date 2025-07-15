b17e01a39c3eaee945bb2311a4d011bbe50389ff
YousseefAh <yousseef.ah@gmail.com>
2025-07-15 16:57:51 +0300

    1canterd hero

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -9,7 +9,7 @@ function Hero() {
     <section className="pt-24 md:pt-28 pb-8 md:pb-16">
       <div className="flex flex-nowrap justify-between md:gap-x-24 lg:gap-x-14">
         <div className="max-w-[50rem]">
-          <h1 className="mb-16 sm:mb-4 font-bold text-3xl text-primary-500 sm:text-4xl md:text-5xl/[3.5rem] lg:text-6xl/[4rem] xl:text-7xl/[5rem] tracking-tight">
+          <h1 className="mb-16 sm:mb-4 font-bold text-3xl text-primary-500 sm:text-4xl md:text-5xl/[3.5rem] lg:text-6xl/[4rem] xl:text-7xl/[5rem] tracking-tight text-center">
             {title}{" "}
                                     <span className="text-yellow-500">
               {highlightedText}
