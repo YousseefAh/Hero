67fc05419358fba574e391ea6c3980c9c9d75454
YoussfAh <yousseef.ah@gmail.com>
2025-07-21 02:08:51 +0300

    video good

--- src/components/sections/Dashboard.jsx
+++ src/components/sections/Dashboard.jsx
@@ -13,9 +13,12 @@ function Dashboard() {
   return (
     <section className="relative">
       <div className="bottom-0 -z-10 absolute bg-primary-500 w-full h-1/2" />
-      <div className="relative justify-items-center grid m-auto px-4 sm:px-8 md:px-16 xl:px-24 py-8 md:py-16 max-w-[90rem] h-auto aspect-[2/1]">
-        {!showVideo ? (
-          <div onClick={handleImageClick} className="cursor-pointer w-full h-full">
+      <div 
+        className="relative justify-items-center grid m-auto px-4 sm:px-8 md:px-16 xl:px-24 py-8 md:py-16 max-w-[90rem] h-auto aspect-[2/1] cursor-pointer"
+        onClick={handleImageClick}
+      >
+        {!showVideo && (
+          <>
             <Image
               src={image.src}
               alt={image.alt}
@@ -23,18 +26,22 @@ function Dashboard() {
               priority
               className="block w-full h-full object-contain rounded-lg"
             />
-          </div>
-        ) : (
-          <div className="aspect-video w-full h-full">
-            <iframe
-              src={`https://www.youtube.com/embed/${videoId}?autoplay=1&rel=0`}
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-              className="w-full h-full rounded-lg"
-            ></iframe>
-          </div>
+            <div className="absolute inset-0 flex items-center justify-center rounded-lg">
+              <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 24 24" fill="white">
+                <path d="M8 5v14l11-7z" />
+              </svg>
+            </div>
+          </>
+        )}
+        {showVideo && (
+          <iframe
+            src={`https://www.youtube.com/embed/${videoId}?autoplay=1&rel=0&modestbranding=1`}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            className="w-full h-full rounded-lg"
+          ></iframe>
         )}
       </div>
     </section>
