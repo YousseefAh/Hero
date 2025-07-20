f5dfb10cf39403513464b415d174a872c754e314
YoussfAh <yousseef.ah@gmail.com>
2025-07-21 02:54:47 +0300

    better centerd

--- src/components/sections/Features.jsx
+++ src/components/sections/Features.jsx
@@ -7,13 +7,13 @@ function Features() {
     <section className="m-auto px-4 sm:px-8 md:px-16 xl:px-24 pt-14 sm:pt-20 md:pt-32 pb-16 md:pb-24 max-w-[90rem]">
       <div className="gap-y-16 md:gap-y-24 grid lg:grid-cols-2">
         <div className="w-[90%] sm:max-w-[80%] md:max-w-[75%] xl:max-w-[65%] mx-auto lg:mx-0 text-center lg:text-left">
-          <h3 className="mb-2 sm:mb-4 font-bold text-[2rem]/[2.5rem] text-primary-500 sm:text-4xl md:text-5xl xl:text-[3.5rem]/[4rem] tracking-tight">
+          <h3 className="text-center inline-block mb-2 sm:mb-4 font-bold text-[2rem]/[2.5rem] text-primary-500 sm:text-4xl md:text-5xl xl:text-[3.5rem]/[4rem] tracking-tight">
             {title}
           </h3>
           <p className="mb-8 md:mb-10 xl:text-lg tracking-tight">
             {description}
           </p>
-          <a href={content.cta.whatsappLink} target="_blank" rel="noopener noreferrer" className="inline-block bg-primary-500 hover:bg-green-500 px-8 py-4 rounded-2xl text-white xl:text-lg tracking-tight transition-all duration-200">
+          <a href={content.cta.whatsappLink} target="_blank" rel="noopener noreferrer" className="text-center inline-block bg-primary-500 hover:bg-green-500 px-8 py-4 rounded-2xl text-white xl:text-lg tracking-tight transition-all duration-200">
             {buttonText}
           </a>
         </div>
