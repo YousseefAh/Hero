05857fd98778cae5bcd98b45db5f29cd72faaa9e
YoussfAh <yousseef.ah@gmail.com>
2025-07-21 18:57:49 +0300

    video link

--- src/data/content.js
+++ src/data/content.js
@@ -249,7 +249,7 @@ export const content = {
       alt: "لوحة التحكم"
     },
     // videoId: "dQw4w9WgXcQ" // Rick Astley - Never Gonna Give You Up
-    videoId: "XN9pE3023jM" 
+    videoId: "IS6bX-59GFM" 
   },
 
   // قسم البطل (Hero)
