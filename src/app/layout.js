5d51bcb5ac4179c310aa35fe702a916bf011226b
YousseefAh <yousseef.ah@gmail.com>
2026-03-06 08:32:02 +0200

    1

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,7 +23,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="ar" dir="rtl">
+    <html lang="ar" dir="rtl" suppressHydrationWarning>
       <body
         className={`${notoKufi.variable} ${spaceGrotesk.variable} font-sans`}
         suppressHydrationWarning
